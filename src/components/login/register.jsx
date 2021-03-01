fef219038e449905ded63c72ecee647e5d1de4b5
Ihor Piontkovskyi <Ihor.Piontkovskyi@McKesson.com>
2021-03-01 18:23:59 +0200

    [GRA-0000] Add register request

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -1,9 +1,56 @@
 import React from "react";
 import loginImg from "../../login.svg";
+import axios from "axios";
+import base64 from "base-64";
 
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      password: '',
+      uid: '',
+      role: 'Student'
+    }
+  }
+
+  myChangeHandleUID = e => {
+    this.setState({uid: e.target.value})
+  }
+
+  myChangeHandlePSWD = e => {
+    this.setState({password: e.target.value})
+  }
+
+  myChangeHandleFirstName = e => {
+    this.setState({firstName: e.target.value})
+  }
+
+  myChangeHandleLastName = e => {
+    this.setState({lastName: e.target.value})
+  }
+  //TODO fix role handling???
+  myChangeHandleRole = e => {
+    this.setState({role: e.target.value})
+  }
+
+  handleRegister = e => {
+    e.preventDefault();
+
+    axios.defaults.baseURL = 'https://service-ems.herokuapp.com/'
+    axios.defaults.withCredentials = true;
+    axios.post('/register',
+        {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          password:  base64.encode(this.state.password),
+          uid: this.state.uid,
+          role: this.state.role
+        }
+    );
+    //TODO add redirect to login
   }
 
   render() {
@@ -16,31 +63,35 @@ export class Register extends React.Component {
           </div>
           <div className="form">
           <div className="form-group">
-              <select name="chooseСharacter">
-                <option value="Lecturer">Lecturer</option>
+              <select name="chooseСharacter" onChange={this.myChangeHandleRole}>
                 <option value="Student">Student</option>
+                <option value="Lecturer">Lecturer</option>
               </select>
             </div>
             <div className="form-group">
               <label htmlFor="username">First Name</label>
-              <input type="text" name="firstName" placeholder="First Name" />
+              <input type="text" name="firstName" placeholder="First Name"
+                     onChange={this.myChangeHandleFirstName}/>
             </div>
             <div className="form-group">
               <label htmlFor="username">Second Name</label>
-              <input type="text" name="secondName" placeholder="Second Name" />
+              <input type="text" name="secondName" placeholder="Second Name"
+                     onChange={this.myChangeHandleLastName}/>
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
+              <input type="text" name="UID" placeholder="UID"
+                     onChange={this.myChangeHandleUID}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password"
+                     onChange={this.myChangeHandlePSWD}/>
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={this.handleRegister}>
             Register
           </button>
         </div>
