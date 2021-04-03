00256352f94ab88e870981f6c091bb5a662ef34c
melnicenkovadik <melnicenkovadik@gmail.com>
2021-04-03 14:33:29 +0300

    add card

--- src/components/main-page/Content.js
+++ src/components/main-page/Content.js
@@ -13,6 +13,7 @@ import { withStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import RefreshIcon from '@material-ui/icons/Refresh';
 
+import Card from "./Card"
 const styles = (theme) => ({
     paper: {
         maxWidth: 936,
@@ -38,7 +39,32 @@ const styles = (theme) => ({
 
 function Content(props) {
     const { classes } = props;
-
+    const turtles = [
+        {
+            name: "Leonardo",
+            nickName: "Leo",
+            weapon: "Katana",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/en/e/ed/Leonardo_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+        },
+        {
+            name: "Donatello",
+            nickName: "Don",
+            weapon: "Bo staff",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/en/5/5a/Donatello_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+        },
+        {
+            name: "Michelangelo",
+            nickName: "Mikey",
+            weapon: "Nunchucks",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/en/f/f3/Michelangelo_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+        },
+        {
+            name: "Raphael",
+            nickName: "Raph",
+            weapon: "Sai",
+            imgUrl: "https://upload.wikimedia.org/wikipedia/en/7/72/Raphael_%28Teenage_Mutant_Ninja_Tutles%29.jpg"
+        }
+    ]
     return (
         <Paper className={classes.paper}>
             <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
@@ -73,6 +99,12 @@ function Content(props) {
             <div className={classes.contentWrapper}>
                 <Typography color="textSecondary" align="center">
                     No users for this project yet
+
+                    {turtles.map((t,index)=>{
+                        return(
+                            <Card title={t.name} weapon={t.weapon} img={t.imgUrl} nickName={t.nickName}/>
+                        )
+                    })}
                 </Typography>
             </div>
         </Paper>
